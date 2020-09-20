5992011b27e5d201822a454c3c33663058220fcc
林煥培 <abc2464831@gmail.com>
2020-09-20 23:38:30 +0800

    password_v1

---
+++ password.py
@@ -0,0 +1,10 @@
+y = 1
+while y <= 3:
+ 	password = input('請輸入密碼：')
+ 	if password == 'a1234567':
+ 		print('密碼正確！')
+ 	else:
+ 		print('請再輸入一次')
+ 		print('密碼錯誤！你還能輸入' , y, '次')
+ 		y = y + 1
+print('請聯絡客服中心')
