1f99eeadb562b9842e7146904b40245353b3ebd9
林煥培 <abc2464831@gmail.com>
2020-10-03 16:54:19 +0800

    continue

--- Product_list.py
+++ Product_list.py
@@ -1,13 +1,17 @@
-#記帳程式清單
+	#記帳程式清單
 products = []
-#讀取檔案 r
+	#讀取檔案 r
 with open('Products_list.csv', 'r', encoding = 'utf-8') as f:
 	for line in f:
+		if '商品,價格' in line:
+			continue 
+			#如果有商品價格的話 直接進入到下一格
 		name, price = line.strip().split(',')
-	products.append([name, price])
+		products.append([name, price])
+		#記得要空格
 print(products)
 
-
+	#輸入商品名稱
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q': #quit
@@ -26,11 +30,13 @@ print('總共有', len(products), '項')
 print(products)
 #print(products[0][1])
 #這樣寫不好 依序把所有產品都列出來可這樣寫
+
+	#印出所以商品
 for p in products:
 #記得要寫對框框！！ p[]!!	
 	print(p[0], '的價格是:',p[1] )
 
-#輸出檔案 w
+	#輸出檔案 w
 with open('Products_list.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
